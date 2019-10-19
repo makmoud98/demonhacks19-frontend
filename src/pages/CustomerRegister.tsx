3fd3176f7af1aaa7631d20e55ba0cedb9f051978
marcinfilipek1 <49353886+marcinfilipek1@users.noreply.github.com>
2019-10-19 17:30:05 -0500

    Create CustomerRegister.tsx

---
+++ src/pages/CustomerRegister.tsx
@@ -0,0 +1,24 @@
+import { IonInput, IonItem, IonLabel,IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonIcon} from '@ionic/react';
+import React from 'react';
+
+const CustomerRegister: React.FC = () => {
+  return (
+    <IonPage>
+      <IonContent className="ion-padding">
+		<IonLabel>E-mail*</IonLabel>
+		<IonInput placeholder="Enter Email"></IonInput>
+		<IonLabel>Password*</IonLabel>
+		<IonInput placeholder="Enter Password"></IonInput>
+		<IonLabel>Age*</IonLabel>
+		<IonInput placeholder="Enter Age"></IonInput>
+		<IonLabel>Gender</IonLabel>
+		<IonInput placeholder="Enter Gender"></IonInput>
+		<IonButton>
+		Submit
+		</IonButton>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default CustomerRegister;
