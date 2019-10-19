247ac6eb366c31f12fee88181477cab62a76d405
marcinfilipek1 <49353886+marcinfilipek1@users.noreply.github.com>
2019-10-19 15:55:21 -0500

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,17 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonIcon} from '@ionic/react';
 import React from 'react';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
+	  <IonButton>
+	  Login
+	  </IonButton>
+	  <br></br>
+	   <IonButton>
+	  Register
+	  </IonButton>
       </IonContent>
     </IonPage>
   );
