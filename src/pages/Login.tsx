714c1d45ff90f820ce140f6a85c72b10f42c887c
MohamedElkarem <56768542+MohamedElkarem@users.noreply.github.com>
2019-10-19 17:45:03 -0500

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,22 +1,17 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonIcon} from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonIcon, IonInput,IonLabel} from '@ionic/react';
 import React from 'react';
 const Login: React.FC = () => {
   return (
   	<IonPage>
       <IonContent className="ion-padding">
-		
 		<IonLabel>Email:</IonLabel>
-		<IonInput type= "text" email= "email"></IonInput>
+		<IonInput placeholder= "email"></IonInput>
 		<IonLabel>Password: </IonLabel>
-		<IonInput type= "text" Password= "Password"></IonInput>
-		<IonButton ="SignIn" </IonButton>
-
-
-
-				
+		<IonInput placeholder= "Password"></IonInput>
+		<IonButton>SignIn </IonButton>	
       </IonContent>
     </IonPage>
   );
 };
 
-export default Register;
+export default Login;
