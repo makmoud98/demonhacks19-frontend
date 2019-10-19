570eccc73519dd6c1ea3d9c93fec3e4d83cdbce1
marcinfilipek1 <49353886+marcinfilipek1@users.noreply.github.com>
2019-10-19 16:35:23 -0500

    Create Register.tsx

---
+++ src/pages/Register.tsx
@@ -0,0 +1,20 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonIcon} from '@ionic/react';
+import React from 'react';
+
+const Register: React.FC = () => {
+  return (
+    <IonPage>
+      <IonContent className="ion-padding">
+		<IonButton>
+		Customer
+		</IonButton>
+			<br></br>
+		<IonButton>
+		Business
+		</IonButton>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Register;
