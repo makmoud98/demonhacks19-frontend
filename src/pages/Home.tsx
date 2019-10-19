a570a91b3d8f34f3065b8fc296b5141d83c168df
marcinfilipek1 <49353886+marcinfilipek1@users.noreply.github.com>
2019-10-19 16:45:51 -0500

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,14 +5,11 @@ const Home: React.FC = () => {
   return (
     <IonPage>
       <IonContent className="ion-padding">
-      <ion-column>
-      	<ion-col height-50 style ="text-align: top">
 	  <IonButton size= "large">
 	  Login
 	  </IonButton>
-	  </ion-col>
 	  <br></br>
-	   <IonButton href="Register"> Register </IonButton>
+	   <IonButton size= "large" href="Register"> Register </IonButton>
       </IonContent>
     </IonPage>
   );
