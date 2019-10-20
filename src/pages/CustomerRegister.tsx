b8e427a0992a2ccd0b0067240605bbd7828b8784
MohamedElkarem <56768542+MohamedElkarem@users.noreply.github.com>
2019-10-19 22:47:03 -0500

    Update CustomerRegister.tsx

--- src/pages/CustomerRegister.tsx
+++ src/pages/CustomerRegister.tsx
@@ -1,27 +1,56 @@
 import { IonInput, IonLabel, IonContent, IonPage, IonButton} from '@ionic/react';
-import React from 'react';
-import {Map, GoogleApiWrapper} from 'google-maps-react';
+import { Redirect } from 'react-router-dom'
+import React from 'react'
+import axios from 'axios'
  
 
 
-const CustomerRegister: React.FC = () => {
-  return (
-    <IonPage>
-      <IonContent className="ion-padding">
-		<IonLabel>E-mail*</IonLabel>
-		<IonInput placeholder="Enter Email"></IonInput>
-		<IonLabel>Password*</IonLabel>
-		<IonInput placeholder="Enter Password"></IonInput>
-		<IonLabel>Age*</IonLabel>
-		<IonInput placeholder="Enter Age"></IonInput>
-		<IonLabel>Gender</IonLabel>
-		<IonInput placeholder="Enter Gender"></IonInput>
-		<IonButton size="large" color="tertiary" shape="round" fill="outline">
-		Submit
-		</IonButton>
-      </IonContent>
-    </IonPage>
-  );
-};
+ 
+class CustomerRegister extends React.Component {
+  state : any;
+
+  constructor() {
+    super({});
+    this.state = {'email': '', 'password': ''};
+  }
+  
+  onClick = () => {
+    axios.post("https://vps258740.vps.ovh.ca/api/auth/register/customer", this.state).then(res => {
+      if(res.status === 200){
+        console.log('success!')
+        return <Redirect to='/CustomerDashboard'/>
+      }
+    })
+  }
+
+  onInputChange = (e) => {
+    var key = e.target.id
+    var val = e.target.value
+    this.setState({[key]: val})
+    console.log(this.state)
+  }
+
+  render() {
+    return (
+      <IonPage>
+        <IonContent className="ion-padding">
+          <IonLabel>E-mail*</IonLabel>
+          <IonInput 
+            id="email"
+            onIonChange={this.onInputChange}
+            placeholder="Enter Email"></IonInput>
+          <IonLabel>Password*</IonLabel>
+          <IonInput 
+            id="password"
+            onIonChange={this.onInputChange}
+            placeholder="Enter Password"></IonInput>
+          <IonButton size="large" color="tertiary" shape="round" fill="outline" href="BusinessDash" onClick={this.onClick}>
+            Submit
+          </IonButton>
+        </IonContent>
+      </IonPage>
+    )
+  }
+}
 
 export default CustomerRegister;
