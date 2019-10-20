ea57f99f15fcd7339df25419f19d8555f0c139ff
MohamedElkarem <56768542+MohamedElkarem@users.noreply.github.com>
2019-10-19 22:47:06 -0500

    Create CustomerDashboard.tsx

---
+++ src/pages/CustomerDashboard.tsx
@@ -0,0 +1,14 @@
+import { IonInput, IonLabel, IonContent, IonPage, IonButton} from '@ionic/react';
+import React from 'react';
+
+const CustomerDashboard: React.FC = () => {
+	return (
+    <IonPage>
+    	<IonContent className="ion-padding">
+	
+      	</IonContent>
+    </IonPage>
+  );
+};
+
+export default CustomerDashboard;
