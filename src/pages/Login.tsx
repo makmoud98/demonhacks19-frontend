1d3a2bec7178a71a9280317051bb0c34e2ddddd6
Michael Elnajami <makmoud98@gmail.com>
2019-10-19 22:53:01 -0400

    made the form actually work in login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,17 +7,32 @@ class Login extends React.Component {
 
   constructor() {
     super({});
-    this.state = {email: '', password: ''};
+    this.state = {'email': '', 'password': ''};
   }
   
   onClick = () => {
-    axios.post("https://vps258740.vps.ovh.ca/api/auth/login", {
-      email: "test@test.com", password: "password"
-    }).then(res => {
+    axios.post("https://vps258740.vps.ovh.ca/api/auth/login", this.state).then(res => {
       if(res.status === 200){
         console.log('success!')
+        var type = res.data.type;
+        if(type == 'consumer'){
+          return <Redirect to='/target' />
+        }
+        else if(type == 'business'){
+
+        }
+        else{
+
+        }
       }
     })
+  }
+
+  onInputChange = (e) => {
+    var key = e.target.id
+    var val = e.target.value
+    this.setState({[key]: val})
+    console.log(this.state)
   }
 
   render() {
@@ -25,10 +40,16 @@ class Login extends React.Component {
       <IonPage>
         <IonContent className="ion-padding">
           <IonLabel>E-mail:</IonLabel>
-          <IonInput placeholder= "email"></IonInput>
+          <IonInput 
+            id="email"
+            onIonChange={this.onInputChange}
+            placeholder="email"/>
           <IonLabel>Password: </IonLabel>
-          <IonInput placeholder= "password"></IonInput>
-          <IonButton onClick={this.onClick}>Sign In </IonButton> 
+          <IonInput 
+            id="password"
+            onIonChange={this.onInputChange}
+            placeholder="password"/>
+          <IonButton onClick={this.onClick}>Sign In </IonButton>
         </IonContent>
       </IonPage>
     )
