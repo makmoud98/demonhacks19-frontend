5be5eac7fbb5827cf3e9660c709fae91cf2bbf96
Michael Elnajami <root@vps258740.vps.ovh.ca>
2019-10-19 21:30:01 -0400

    fixed warnings and fixed login page

--- src/pages/BusinessRegister.tsx
+++ src/pages/BusinessRegister.tsx
@@ -1,4 +1,4 @@
-import { IonInput, IonItem, IonLabel,IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonIcon} from '@ionic/react';
+import { IonInput, IonLabel, IonContent, IonPage, IonButton} from '@ionic/react';
 import React from 'react';
 
 const BusinessRegister: React.FC = () => {
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonInput, IonLabel, IonButton, IonIcon} from '@ionic/react';
+import { IonContent, IonPage, IonButton} from '@ionic/react';
 import React from 'react';
 
 const Home: React.FC = () => {
