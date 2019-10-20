9856b94101b21c563a0259bc555aa27850a22d1c
mstromidlo <mattstromidlo@gmail.com>
2019-10-19 21:52:09 -0500

    Create BusinessDash.tsx

---
+++ src/pages/BusinessDash.tsx
@@ -0,0 +1,13 @@
+import { IonContent, IonPage, IonButton} from '@ionic/react';
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonContent className="ion-padding">
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
